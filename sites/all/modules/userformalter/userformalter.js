47b8d97d45c5111abff1d8aca502d314c927ebb5
Lindsay Wilson <web@origindesign.ca>
2014-02-19 19:44:40 -0800

    client section, news/notes, new people view

--- sites/all/modules/userformalter/userformalter.js
+++ sites/all/modules/userformalter/userformalter.js
@@ -1,41 +1,29 @@
 jQuery(document).ready(function($){
 	
-	list = $('#edit-field-client-und');
-	list2 = $('#edit-field-adv-und');
-	  
-	list.children().each(function(){
-		if($(this).attr('value')!='_none'){
-			txt = $(this).text().split(/ (.+)?/);
-			if(typeof txt[1] != 'undefined'){
-				$(this).text(txt[1]+', '+txt[0]);
+	
+	// Lists to reorder
+	lists = new Array('edit-field-client-und', 'edit-field-adv-und', 'edit-field-visible-to-advisors-und', 'edit-field-visible-to-clients-und');
+	
+	$.each(lists, function( index, value ) {
+		
+		// Set as lastname, firstname
+		$('#'+value).children().each(function(){
+			if($(this).attr('value')!='_none'){
+				txt = $(this).text().split(/ (.+)?/);
+				if(typeof txt[1] != 'undefined'){
+					$(this).text(txt[1]+', '+txt[0]);
+				}
 			}
-		}
-	});
-	
-	list2.children().each(function(){
-		if($(this).attr('value')!='_none'){
-			txt = $(this).text().split(/ (.+)?/);
-			if(typeof txt[1] != 'undefined'){
-				$(this).text(txt[1]+', '+txt[0]);
-			}
-		}
-	});
-  
-	
-	var options = $('#edit-field-client-und option');
-	var arr = options.map(function(_, o) { return { t: $(o).text(), v: o.value }; }).get();
-	arr.sort(function(o1, o2) { return o1.t > o2.t ? 1 : o1.t < o2.t ? -1 : 0; });
-	options.each(function(i, o) {
-	  o.value = arr[i].v;
-	  $(o).text(arr[i].t);
-	});
-	
-	var options = $('#edit-field-adv-und option');
-	var arr = options.map(function(_, o) { return { t: $(o).text(), v: o.value }; }).get();
-	arr.sort(function(o1, o2) { return o1.t > o2.t ? 1 : o1.t < o2.t ? -1 : 0; });
-	options.each(function(i, o) {
-	  o.value = arr[i].v;
-	  $(o).text(arr[i].t);
+		});
+		
+		// Reorder alphabetically
+		var options = $('#'+value).find('option');
+		var arr = options.map(function(_, o) { return { t: $(o).text(), v: o.value }; }).get();
+		arr.sort(function(o1, o2) { return o1.t > o2.t ? 1 : o1.t < o2.t ? -1 : 0; });
+		options.each(function(i, o) {
+		  o.value = arr[i].v;
+		  $(o).text(arr[i].t);
+		});
 	});
 
 
