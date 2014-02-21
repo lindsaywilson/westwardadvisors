8a6df2e2d70ce758f3a676c7ba881a99df1afdfc
Lindsay Wilson <web@origindesign.ca>
2014-02-20 18:46:53 -0800

    reorder people list and all edit screens

--- sites/all/modules/userformalter/userformalter.js
+++ sites/all/modules/userformalter/userformalter.js
@@ -2,7 +2,7 @@ jQuery(document).ready(function($){
 	
 	
 	// Lists to reorder
-	lists = new Array('edit-field-client-und', 'edit-field-adv-und', 'edit-field-visible-to-advisors-und', 'edit-field-visible-to-clients-und');
+	lists = new Array('edit-field-client-und', 'edit-field-adv-und', 'edit-field-visible-to-advisors-und', 'edit-field-visible-to-clients-und', 'edit-field-advisor-und');
 	
 	$.each(lists, function( index, value ) {
 		
@@ -18,11 +18,24 @@ jQuery(document).ready(function($){
 		
 		// Reorder alphabetically
 		var options = $('#'+value).find('option');
+		selected = Array();
+		$('#'+value).find(":selected").each( function(){
+			selected.push($(this).val());
+		});
 		var arr = options.map(function(_, o) { return { t: $(o).text(), v: o.value }; }).get();
 		arr.sort(function(o1, o2) { return o1.t > o2.t ? 1 : o1.t < o2.t ? -1 : 0; });
 		options.each(function(i, o) {
 		  o.value = arr[i].v;
 		  $(o).text(arr[i].t);
+		});
+		options.each( function(){
+			option = $(this);
+			option.attr('selected','');
+			$.each(selected, function( index, value ) {
+				if(value == option.val()){
+					option.attr('selected', 'selected');
+				}
+			});
 		});
 	});
 
