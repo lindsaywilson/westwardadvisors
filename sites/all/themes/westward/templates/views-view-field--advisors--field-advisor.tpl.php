45f6d88d336162c2db28a6ddafefb4a1fcc33881
Lindsay Wilson <web@origindesign.ca>
2014-03-23 17:14:59 -0700

    client column

---
+++ sites/all/themes/westward/templates/views-view-field--advisors--field-advisor.tpl.php
@@ -0,0 +1,49 @@
+<?php
+
+/**
+ * @file
+ * This template is used to print a single field in a view.
+ *
+ * It is not actually used in default Views, as this is registered as a theme
+ * function which has better performance. For single overrides, the template is
+ * perfectly okay.
+ *
+ * Variables available:
+ * - $view: The view object
+ * - $field: The field handler object that can process the input
+ * - $row: The raw SQL result that can be used
+ * - $output: The processed output that will normally be used.
+ *
+ * When fetching output from the $row, this construct should be used:
+ * $data = $row->{$field->field_alias}
+ *
+ * The above will guarantee that you'll always get the correct data,
+ * regardless of any changes in the aliasing that might happen if
+ * the view is modified.
+ */
+?>
+<ul>
+<?php 
+
+foreach( $row->field_field_advisor as &$uid){
+
+	$user = user_load($uid['raw']['uid']);
+	$firstname = '';
+	if(isset($user->field_first_name['und'])){
+		$firstname = $user->field_first_name['und'][0]['value'];
+	}
+	$lastname = '';
+	if(isset($user->field_last_name['und'])){
+		$lastname = $user->field_last_name['und'][0]['value'];
+	}
+	$company = '';
+	if(isset($user->field_company['und'])){
+		$company = $user->field_company['und'][0]['value'];
+	}
+	
+	print '<li>'.$firstname.' '.$lastname.' - '. $company.'</li>';
+
+}
+
+?>
+</ul>
