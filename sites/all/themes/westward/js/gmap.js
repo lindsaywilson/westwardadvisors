9ad2189d59a374368f313afc7a489ad86c624640
Lindsay Wilson <web@origindesign.ca>
2014-07-08 20:41:09 -0700

    address update on gmap

--- sites/all/themes/westward/js/gmap.js
+++ sites/all/themes/westward/js/gmap.js
@@ -1,13 +1,13 @@
 function initialize() {
         var myOptions = {
-          center: new google.maps.LatLng(49.28465266206896, -123.12171936035156),
+          center: new google.maps.LatLng(49.276513,-123.121333),
           zoom: 15,
 		  mapTypeControl: false,
           mapTypeId: google.maps.MapTypeId.ROADMAP		  
         };
         var map = new google.maps.Map(document.getElementById("map"),
             myOptions);
-		var coords = new google.maps.LatLng(49.28465266206896, -123.12171936035156);	
+		var coords = new google.maps.LatLng(49.276513,-123.121333);	
 		var marker = new google.maps.Marker({
 		  position: coords,
 		  map: map,
